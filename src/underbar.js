245035e97f838516e894b61539718bd52b7558c8
codengjon <1jonathan.ng@gmail.com>
2014-06-05 23:43:56 -0700

    uniq complete

--- src/underbar.js
+++ src/underbar.js
@@ -98,9 +98,17 @@ var _ = {};
             }
       });
       return newArr;
-    }
+    };
+
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var newArr =[];
+    _.each(array, function(item){
+        if(_.indexOf(newArr,item)===-1){
+          newArr.push(item);
+        }
+    })
+    return newArr;
   };
 
 
